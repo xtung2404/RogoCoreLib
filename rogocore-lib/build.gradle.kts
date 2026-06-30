72e1b4255f8905cf3d15f13fec4c83f7c6cedd2e
Phạm Xuân Tùng <77226010+xtung2404@users.noreply.github.com>
2026-06-30 14:26:14 +0700

    1.0.3.17
    - handle duplicate subcribe to mqtt topic

--- rogocore-lib/build.gradle.kts
+++ rogocore-lib/build.gradle.kts
@@ -71,7 +71,7 @@ afterEvaluate {
 
                 groupId = "com.github.xtung2404"
                 artifactId = "RogoCoreLib"
-                version = "1.0.3.16" // Tăng version để JitPack nhận bản mới nhất
+                version = "1.0.3.17" // Tăng version để JitPack nhận bản mới nhất
             }
         }
     }
